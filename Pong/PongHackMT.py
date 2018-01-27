cbedb8b0426815d5b0216096c6a215bb8a37488e
Jesse Offei-Nkansah <jeoffei@gmail.com>
2018-01-27 00:33:17 -0600

    fixed window sizing

--- Pong/PongHackMT.py
+++ Pong/PongHackMT.py
@@ -3,11 +3,14 @@
 import pygame, sys
 from pygame.locals import *
 
+WINDOW_HEIGHT = 210
+WINDOW_WIDTH = 160
+
 #initialise the pygame module
 pygame.init()
 
 # set up display size
-windowDisplay = pygame.display.set_mode((210, 160))
+windowDisplay = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 
 #Title
 pygame.display.set_caption("PongHackMT")
