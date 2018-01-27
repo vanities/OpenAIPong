1da5601a2ef693326c4f314550a72f5d44852b51
Noah Pennington <pennington.noah@gmail.com>
2018-01-27 00:45:36 -0600

    Added functions that will flatten our frame data from the gym's pong render

--- testpong.py
+++ testpong.py
@@ -1,6 +1,78 @@
 import gym
+import keras
+import numpy as np
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+
+input_dimensions = 80 * 80
+prev_processed_observations = None
+
+
+def downsample(image):
+    # Take only alternate pixels - basically halves the resolution of the image (which is fine for us)
+    return image[::2, ::2, :]
+
+def remove_color(image):
+    """Convert all color (RGB is the third dimension in the image)"""
+    return image[:, :, 0]
+
+def remove_background(image):
+    image[image == 144] = 0
+    image[image == 109] = 0
+    return image
+
+def preprocess_observations(input_observation, prev_processed_observation, input_dimensions):
+    """ convert the 210x160x3 uint8 frame into a 6400 float vector """
+    processed_observation = input_observation[35:195] # crop
+    processed_observation = downsample(processed_observation)
+    processed_observation = remove_color(processed_observation)
+    processed_observation = remove_background(processed_observation)
+    processed_observation[processed_observation != 0] = 1 # everything else (paddles, ball) just set to 1
+    # Convert from 80 x 80 matrix to 1600 x 1 matrix
+    processed_observation = processed_observation.astype(np.float).ravel()
+
+    # subtract the previous frame from the current one so we are only processing on changes in the game
+    if prev_processed_observation is not None:
+        input_observation = processed_observation - prev_processed_observation
+    else:
+        input_observation = np.zeros(input_dimensions)
+    # store the previous frame so we can subtract from it next time
+    prev_processed_observations = processed_observation
+    return input_observation, prev_processed_observations
+
+
+# create gym pong session
 env = gym.make('Pong-v0')
-env.reset()
-for _ in range(1000):
+observation = env.reset()
+
+# continually render the pong instance
+while True:
     env.render()
-    env.step(env.action_space.sample()) # take a random action?
+    # inputting actions, this is just a sample
+    action = env.action_space.sample()
+    # this function flattens each frame
+    processed_observations, prev_processed_observations = preprocess_observations(observation, prev_processed_observations, input_dimensions)
+    # provides new step information
+    observation, reward, done, info = env.step(action)
+    
+
+
+
+# env = gym.make('Pong-v0')
+# for i_episode in range(20):
+#     observation = env.reset()
+#     for t in range (100):
+#         env.render()
+#         print(observation)
+#         action = env.action_space.sample()
+#         observation, reward, done, info = env.step(action)
+#         if done:
+#             print("Episode finished after {} timesteps".format(t+1))
+#             break
+
+#env.reset()
+# for _ in range(1000):
+#     env.render()
+#     env.step(env.action_space.sample()) # take a random action?
